d147ef5e32de487f3c4fbf35eeec79806c98c230
Alberto AC <alberto.avacerva@gmail.com>
2020-08-29 17:07:03 -0700

    client side loader and axios integration

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ interface IProps {
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
@@ -15,6 +16,7 @@ const ActivityForm: React.FC<IProps> = ({
     activity: initialFormState,
     createActivity,
     editActivity,
+    submitting,
 }) => {
     const intializeForm = () => {
         if (initialFormState) {
@@ -95,7 +97,7 @@ const ActivityForm: React.FC<IProps> = ({
                     name='venue'
                     onChange={handleInputChange}
                 />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button
                     onClick={() => setEditMode(false)}
                     floated='right'
